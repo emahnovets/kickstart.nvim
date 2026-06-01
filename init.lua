b90b961f88e1e7431d415d05e225f7116df494c5
Evgeny Mahnovets <mail@emahnovets.dev>
2026-06-01 08:14:13 +0200

    Install rust plugin to nvim

--- init.lua
+++ init.lua
@@ -1002,6 +1002,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.harpoon',
   require 'kickstart.plugins.copilot',
   require 'kickstart.plugins.vim-tmux-navigator',
+  require 'kickstart.plugins.rustaceanvim',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
